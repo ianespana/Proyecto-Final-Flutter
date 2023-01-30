56d5c679ff1201daac8e366eb4fc314ac786846f
Ian Ramirez-España <ianespana@users.noreply.github.com>
2023-01-30 12:48:25 -0600

    Usar GridView.count

--- lib/achievements.dart
+++ lib/achievements.dart
@@ -7,12 +7,19 @@ class Achievements extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Logros"),
+        backgroundColor: Colors.transparent,
+        bottomOpacity: 0.0,
+        elevation: 0.0,
+      ),
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: GridView.extent(
-          maxCrossAxisExtent: 150,
+      body: GridView.count(
           padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-          mainAxisSpacing: 4,
-          crossAxisSpacing: 4,
+          childAspectRatio: 1,
+          crossAxisCount: 3,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
           children: const [
             Achievement(
                 name: 'Racha de una semana',
